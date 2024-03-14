4b2e5f31aab6aacbdfefa23e9e7d8a7d7a242fb0
NyczDev <nicolasyasumoto@gmail.com>
2024-03-14 10:03:48 -0300

    update ex18

--- Le01/src/Exercicio17.java
+++ Le01/src/Exercicio17.java
@@ -4,19 +4,16 @@ public class Exercicio17 {
     public static void executar() {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite a primeira nota do aluno:");
+        System.out.println("Digite a nota e peso 1, do aluno:");
         double nota1 = sc.nextDouble();
-        System.out.println("Digite o peso da primeira nota do aluno:");
         double peso1 = sc.nextDouble();
 
-        System.out.println("Digite a segunda nota do aluno:");
+        System.out.println("Digite a nota e peso 2, do aluno:");
         double nota2 = sc.nextDouble();
-        System.out.println("Digite o peso da segunda nota do aluno:");
         double peso2 = sc.nextDouble();
 
-        System.out.println("Digite a terceira nota do aluno:");
+        System.out.println("Digite a nota e peso 3, do aluno:");
         double nota3 = sc.nextDouble();
-        System.out.println("Digite o peso da terceira nota do aluno:");
         double peso3 = sc.nextDouble();
 
         double mediaPonderada = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3);
---
+++ Le01/src/Exercicio18.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Exercicio18 {
+    public static void executar() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota do aluno:");
+        double nota1 = sc.nextDouble();
+
+        System.out.println("Digite a segunda nota do aluno:");
+        double nota2 = sc.nextDouble();
+
+        System.out.println("Digite a terceira nota do aluno:");
+        double nota3 = sc.nextDouble();
+
+        double mediaHarmonica = 3 / (1/nota1 + 1/nota2 + 1/nota3);
+
+        System.out.printf("A média harmônica das notas %.2f, %.2f e %.2f é: %.2f\n", nota1, nota2, nota3, mediaHarmonica);
+    }
+}
